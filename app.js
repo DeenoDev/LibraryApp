ae1fe72cb4933a3a434ea0a1851c9b12279cda02
DeenoDev <deyanoklaasen@outlook.com>
2022-10-13 23:12:49 +0200

    completed Add Book to UI function that accepts inputs from form.

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ class UI {
                 author: 'Jane Doe',
                 isbn: '45545'
 
-            }
+            },
         ];
 
         const books = StoredBooks;
@@ -29,26 +29,49 @@ class UI {
         books.forEach((book) => UI.addBookToList(book));
     }
 
-    static addBookToList() {
+    static addBookToList(Book) {
         const list = document.querySelector('#book-list');
 
         const row = document.createElement('tr');
 
         row.innerHTML = `
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete"></a></td>
+        <td>${Book.title}</td>
+        <td>${Book.author}</td>
+        <td>${Book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
 
         `;
+
+        list.appendChild(row);
     }
 }
 
 // Store Class: Handles Storage
 
 // Events: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book 
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    //Prevent actual submit
+    e.preventDefault();
+
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Instantiate Book
+    const book = new Book(title, author, isbn);
+
+    // Add Book to UI
+    UI.addBookToList(book);
+
+    // Clear fields
+    UI.clearFields();
+
+    
+});
 
 // Event: Remove a Book
 
