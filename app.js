a670ae4e41c1ce04509f09597349f69c9f99cc38
DeenoDev <deyanoklaasen@outlook.com>
2022-10-14 22:55:40 +0200

    added deleteBook funtion to UI class.

--- app.js
+++ app.js
@@ -45,6 +45,12 @@ class UI {
         list.appendChild(row);
     }
 
+    static deleteBook(el) {
+        if(el.classList.contains('delete')){
+            el.parentElement.parentElement.remove();
+        }
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -80,5 +86,8 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 });
 
 // Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    UI.deleteBook(e.target)
+});
 
 
