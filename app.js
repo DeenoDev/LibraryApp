dedd3b2939ba344fbb50674308277cba85543474
DeenoDev <deyanoklaasen@outlook.com>
2022-10-14 23:25:19 +0200

    added remove alert message after 2 seconds.

--- app.js
+++ app.js
@@ -51,6 +51,18 @@ class UI {
         }
     }
 
+    static showAlert(message, className){
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        // Vanish in 2 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 2000);
+
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -75,7 +87,7 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 
     // Validate
     if(title === '' || author === '' || isbn === '') {
-        alert('Please fill in all fields');
+        UI.showAlert('Please fill in all fields', 'danger');
     } else {
 
     // Instantiate Book
